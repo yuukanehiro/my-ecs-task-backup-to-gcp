147d1a0aa6923478b0a1ed56983ef413ca9d3756
yuu kanehiro <info@lpb-studio.com>
2022-11-19 01:23:06 +0900

    my-ecs-task-backup-to-gcp Public

---
+++ README.md
@@ -0,0 +1,1 @@
+# Backup To Gcp
---
+++ environments/develop/docker/GCLOUD_SDK/sync_from_s3_to_gcs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -e "[Credentials]\naws_access_key_id = ${AWS_ACCESS_KEY_ID}\naws_secret_access_key = ${AWS_ACCESS_KEY_SECRET}\n\n[s3]\nuse-signv4=True\nhost=s3.ap-northeast-1.amazonaws.com" > /tmp/.boto
+
+echo "export BOTO_CONFIG start"
+export BOTO_CONFIG=/tmp/.boto
+echo "export BOTO_CONFIG end"
+
+echo "gcloud auth start"
+gcloud auth activate-service-account --key-file /usr/src/app/service_account.json
+echo "gcloud auth end"
+
+echo "gsutil rsync start"
+gsutil -m cp -r s3://sample-backup-develop/ gs://sample-backup-develop/
+echo "gsutil rsync end"
